bf009ddc8f869717f84cea0a4b885819e7b118ef
Chase Jordan Nel <123620591+Chase-Nel@users.noreply.github.com>
2023-07-20 08:59:52 +0200

    Update InquiryCreatedEvent.kt

--- Backend/dependency-kotlin/inquiry/src/main/kotlin/com/staffinghub/coding/challenges/dependency/inquiry/InquiryCreatedEvent.kt
+++ Backend/dependency-kotlin/inquiry/src/main/kotlin/com/staffinghub/coding/challenges/dependency/inquiry/InquiryCreatedEvent.kt
@@ -6,4 +6,3 @@ import org.springframework.context.ApplicationEvent
  * This class represents an event that is triggered when an inquiry is created.
  */
 class InquiryCreatedEvent(source: Any, val inquiry: Inquiry) : ApplicationEvent(source)
-
